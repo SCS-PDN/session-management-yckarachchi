af755db6bfc1a5016557ae7fb6113d58010bac15
Yasiru Chathuranga <s20308@sci.pdn.ac.lk>
2025-05-02 15:38:49 +0530

    FInal Course.java

---
+++ Course.java
@@ -0,0 +1,17 @@
+package com.assignment4;
+
+public class Course implements java.io.Serializable {
+    private String courseId;
+    private String courseName;
+    private String instructor;
+
+    public Course(String courseId, String courseName, String instructor) {
+        this.courseId = courseId;
+        this.courseName = courseName;
+        this.instructor = instructor;
+    }
+
+    public String getCourseId() { return courseId; }
+    public String getCourseName() { return courseName; }
+    public String getInstructor() { return instructor; }
+}
